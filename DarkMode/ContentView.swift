048a24cf9eba608ee807573da2e7b008a95292d9
Aishwarya <aishwaryarana2807@gmail.com>
2026-02-09 15:20:11 +0530

    Initial Commit

---
+++ DarkMode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DarkMode
+//
+//  Created by Aishwarya Rana on 09/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DarkMode/DarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeApp.swift
+//  DarkMode
+//
+//  Created by Aishwarya Rana on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
